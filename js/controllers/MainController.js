97fac17e36ffb8f46a3a8fdcc58e1431e8eb5e9c
Juan Jimenez <bozz212@gmail.com>
2022-01-06 01:01:52 -0800

    new update

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,19 +1,24 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.program = {
-    series: "Sherlock",
-    series_img: "https://content.codecademy.com/projects/4/bolt-network-2/img/sherlock.jpg",
+    series: "Scarface",
+    series_img: "https://www.gruv.com/assets/images/site/product/61029798.jpg",
     genre: "Crime drama",
-    season: 3,
-    episode: "The Empty Hearse",
-    description: "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
-    datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
+    description: "Scarface is a 1983 American crime drama film directed by Brian De Palma and written by Oliver Stone. A remake of the 1932 film of the same name, it tells the story of Cuban refugee Tony Montana (Al Pacino), who arrives penniless in 1980s Miami and goes on to become a powerful drug lord.",
+    datetime: new Date(1989, 11, 31, 21, 00, 00, 00)
   }
   $scope.anothershow = {
-    series: "Always Sunny",
-    series_img: "http://ecx.images-amazon.com/images/I/510tRaJAfJL.jpg",
-    genre: "Crime drama",
-    season: 2,
-    episode: "Day Man",
-    description: "Funny Man has to box with someone to keep his reputation and be victorious god is jusgandote.",
-    datetime: new Date(2015, 11, 31, 21, 00, 00, 00) }
+    series: "Castlevania",
+    series_img: "https://s.ecrater.com/stores/340271/59f348997fbc9_340271b.jpg",
+    genre: "horror",
+    season: 1,
+    episode: "Witchbottle",
+    description: "A headstrong young woman seeks out forbidden knowledge; A corrupt bishop courts disaster by enraging a legend.",
+    datetime: new Date(2021, 11, 31, 21, 00, 00, 00) }
+    
+    $scope.anothershow1 = {
+      series: "Tupac Ressurection",
+      series_img: "https://th.bing.com/th/id/OIP.YjwooDW3pYTkNqRgF7RYaQHaKs?pid=ImgDet&rs=1",
+      genre: "Biography",
+      description: "Tupac: Resurrection is a 2003 American documentary film about the life and death of rapper Tupac Shakur. The film, directed by Lauren Lazin and released by Paramount Pictures, is narrated by Tupac Shakur himself. The film was in theaters from November 16, 2003 to December 21, 2003.",
+      datetime: new Date(2003, 11, 21, 21, 00, 00, 00) }
   }]);
